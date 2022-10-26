c24c33bebf0edf7b9812066b39244f2dbe6f5875
João Agostinho <joao.agostinho900@hotmail.com>
2022-10-27 00:01:08 +0100

    add-main-program
    add-main-program

---
+++ scheduler/src/main/java/com/jagostinho/Scheduler.java
@@ -0,0 +1,11 @@
+package com.jagostinho;
+
+import com.jagostinho.scheduler_api.SchedulerApplication;
+import com.jagostinho.scheduler_app.SchedulerMain;
+
+public class Scheduler {
+    public static void main(String[] args) {
+        SchedulerApplication.main(args);
+        SchedulerMain.main(args);
+    }
+}
