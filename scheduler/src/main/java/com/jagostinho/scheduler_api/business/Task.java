3266d253bb6100e0ddf64cf85ff1d59b93ee84b2
João Agostinho <joao.agostinho900@hotmail.com>
2022-10-26 18:23:05 +0100

    apply-lombok-notations
    apply-lombok-notations

--- scheduler/src/main/java/com/jagostinho/scheduler_api/business/Task.java
+++ scheduler/src/main/java/com/jagostinho/scheduler_api/business/Task.java
@@ -16,11 +16,6 @@ public class Task implements Serializable {
 
     // #region Properties
 
-    // @Id
-    // @SequenceGenerator(name = "task_sequence", sequenceName = "task_sequence",
-    // allocationSize = 1)
-    // @GeneratedValue(strategy = GenerationType.SEQUENCE, generator =
-    // "task_sequence")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -46,10 +41,6 @@ public class Task implements Serializable {
 
     // #region Constructors
 
-    // public Task() {
-
-    // }
-
     public Task(Long id, Date createdAt, Date updatedAt, Date dueDate, Date resolvedAt, String title,
             String description, Integer priority, Boolean status) {
         this.id = id;
@@ -74,118 +65,6 @@ public class Task implements Serializable {
         this.priority = priority;
         this.status = status;
     }
-
-    // #endregion
-
-    // #region Getters and Setters
-
-    // #region Id
-
-    // public Long getId() {
-    // return id;
-    // }
-
-    // public void setId(Long id) {
-    // this.id = id;
-    // }
-
-    // // #endregion
-
-    // // #region CreatedAt
-
-    // public Date getCreatedAt() {
-    // return createdAt;
-    // }
-
-    // public void setCreatedAt(Date createdAt) {
-    // this.createdAt = createdAt;
-    // }
-
-    // // #endregion
-
-    // // #region UpdatedAt
-
-    // public Date getUpdatedAt() {
-    // return updatedAt;
-    // }
-
-    // public void setUpdatedAt(Date updatedAt) {
-    // this.updatedAt = updatedAt;
-    // }
-
-    // // #endregion
-
-    // // #region DueDate
-
-    // public Date getDueDate() {
-    // return dueDate;
-    // }
-
-    // public void setDueDate(Date dueDate) {
-    // this.dueDate = dueDate;
-    // }
-
-    // // #endregion
-
-    // // #region ResolvedAt
-
-    // public Date getResolvedAt() {
-    // return resolvedAt;
-    // }
-
-    // public void setResolvedAt(Date resolvedAt) {
-    // this.resolvedAt = resolvedAt;
-    // }
-
-    // // #endregion
-
-    // // #region Title
-
-    // public String getTitle() {
-    // return title;
-    // }
-
-    // public void setTitle(String title) {
-    // this.title = title;
-    // }
-
-    // // #endregion
-
-    // // #region Description
-
-    // public String getDescription() {
-    // return description;
-    // }
-
-    // public void setDescription(String description) {
-    // this.description = description;
-    // }
-
-    // // #endregion
-
-    // // #region Priority
-
-    // public Integer getPriority() {
-    // return priority;
-    // }
-
-    // public void setPriority(Integer priority) {
-    // this.priority = priority;
-    // }
-
-    // // #endregion
-
-    // // #region Status
-
-    // public Boolean getStatus() {
-    // return status;
-    // }
-
-    // public void setStatus(Boolean status) {
-    // this.status = status;
-    // }
-
-    // #endregion
 
     // #endregion
 
